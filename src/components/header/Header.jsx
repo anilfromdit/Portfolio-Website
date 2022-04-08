3865a39d46b4d05ffe7af45e4b232d0c701410d8
anilfromdit <anilfromdit@gmail.com>
2022-04-08 20:18:37 +0530

    added typewriter effect, updated main image and some other minor changes

--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -4,13 +4,42 @@ import CallToAction from './CallToAction'
 import Me from '../../assets/me.webp'
 import {AiOutlineArrowRight} from 'react-icons/ai'
 import MySocials from './MySocials'
+import Typewriter from 'typewriter-effect';
 const Header = () => {
   return (
     <header>
       <div className="container header__container">
         <h5 className='kindOfGray' >Hello,I am</h5>
-        <h1 className='kindOfGray' >Anil Gulati</h1>
-        <h5 className='text-light'>Full Stack Developer</h5>
+        <h1 style={{color:'black'}}>Anil Gulati</h1>
+      
+<Typewriter 
+
+options={
+  {
+    autoStart:true,
+    loop:true,
+    wrapperClassName:'primaryColor',
+    cursorClassName:'primaryColor',
+    
+  }
+}
+  onInit={(typewriter) => {
+    typewriter.typeString('I am a Full Stack Developer! ')
+    .changeDeleteSpeed(10)
+    .pauseFor(200)
+    .deleteChars(24)
+    typewriter.typeString('an Android App Developer! ')
+    .pauseFor(200)
+    .deleteAll()
+    typewriter.typeString('2 Years of Experience! ')
+    .pauseFor(200)
+    .deleteAll()
+    typewriter.typeString('Always learning new things! ')
+    .pauseFor(200)
+    .deleteAll()
+    .start();
+  }}
+/>
         <CallToAction/>
         <MySocials/>
         <div className="ME">
